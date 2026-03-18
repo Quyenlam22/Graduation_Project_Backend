356de90684365a22e821bae99e04c9429a8ab679
ledaoquyen2004@gmail.com <ledaoquyen2004@gmail.com>
2026-03-18 14:38:46 +0700

    Add search logic

--- api/v1/routes/index.js
+++ api/v1/routes/index.js
@@ -4,6 +4,7 @@ const songRoute = require("./song.route");
 const artistRoute = require("./artist.route");
 const albumRoute = require("./album.route");
 const playlistRoute = require("./playlist.route");
+const searchRoute = require("./search.route");
 
 module.exports = (app) => {
   const version = `/api/v1`;
@@ -14,4 +15,5 @@ module.exports = (app) => {
   app.use(`${version}/artists`, artistRoute);
   app.use(`${version}/albums`, albumRoute);
   app.use(`${version}/playlists`, playlistRoute);
+  app.use(`${version}/search`, searchRoute);
 }
---
+++ api/v1/routes/search.route.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const route = express.Router();
+
+route.get('/external', async (req, res) => {
+    try {
+        const { q } = req.query;
+        if (!q) return res.json({ data: [] });
+
+        const response = await fetch(`https://api.deezer.com/search?q=${encodeURIComponent(q)}`);
+        const data = await response.json();
+        res.json(data); 
+        
+    } catch (error) {
+        console.error("Backend Error:", error);
+        res.status(500).json({ error: "Error when calling Deezer API", details: error.message });
+    }
+});
+
+module.exports = route;
